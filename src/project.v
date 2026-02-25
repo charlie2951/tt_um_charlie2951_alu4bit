d23bc7f1e874cc2f17931e904b45c1bb42a0251f
charlie2951 <90516512+charlie2951@users.noreply.github.com>
2026-02-25 09:54:53 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_charlie2951_alu4bit (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -22,6 +22,6 @@ module tt_um_example (
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{ena, clk, rst_n,uio_in[3],uio_in[4],uio_in[5],uio_in[6],uio_in[7], 1'b0};
 
 endmodule
